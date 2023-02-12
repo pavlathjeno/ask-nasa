0bb8f4f54002e6ec464548fb941d0d1f91db8746
pavlathjeno <pavlathjeno@gmail.com>
2023-02-12 15:57:03 +0100

    Create root layout with navbar

--- ask-nasa/src/main.jsx
+++ ask-nasa/src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import Root from './routes/Root';
+import Apod from './routes/Apod';
+import Gallery from './routes/Gallery';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+            index: true,
+            element: <Apod/>
+        },
+        {
+            path: "apod",
+            element: <Apod/>
+        },
+        {
+            path: "gallery",
+            element: <Gallery/>
+        },
+      ]
+    },
+  ]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
